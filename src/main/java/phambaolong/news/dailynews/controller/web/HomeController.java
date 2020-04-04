f2db40f8e64481d8f9d826f03050de84304cb2dc
devvdevv <61457360+devvdevv@users.noreply.github.com>
2020-04-04 17:15:14 +0700

    Redirect to /home

--- src/main/java/phambaolong/news/dailynews/controller/web/HomeController.java
+++ src/main/java/phambaolong/news/dailynews/controller/web/HomeController.java
@@ -1,18 +1,22 @@
 package phambaolong.news.dailynews.controller.web;
 
+import java.io.IOException;
+
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = {"/"})
+@WebServlet(urlPatterns = {"/home"})
 public class HomeController extends HttpServlet {
 
 	private static final long serialVersionUID = -8064909454214279743L;
 	
-	protected void doGet(HttpServletRequest resquest, HttpServletResponse response) {
-		RequestDispatcher rd = resquest.getRequestDispatcher("");
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd = request.getRequestDispatcher("/views/web/home.jsp");
+		rd.forward(request, response);
 	}
 	
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,12 +1,2 @@
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="ISO-8859-1">
-<title>Insert title here</title>
-</head>
-<body>
-	<h1>Xin chao!</h1>
-</body>
-</html>
+<%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core" %>
+<c:redirect url="/home" />
